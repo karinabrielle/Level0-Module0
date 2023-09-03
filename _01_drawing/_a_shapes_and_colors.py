a828bc7875d56e6a2dd88ced86b15b2566a90adf
league <karinabriellen@gmail.com>
2023-09-03 12:53:37 -0700

    turtle project :)

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,14 +5,15 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # This code makes a new Turtle. Pick a new name for the turtle
-    my_turtle = turtle.Turtle()
+    sally = turtle.Turtle()
 
     # Make your turtle's shape 'turtle', .shape('turtle')
-
-    # Set your turtle's speed using .speed(2)
-
+    sally.shape('turtle')
+    # Set your turtle's speed using .speed(10)
+    sally.speed(10)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    sally.color('green')
+    sally.pencolor('blue')
     # Move your turtle forward using .forward(100)
     # TEST    Did your turtle move forward?
 
@@ -20,16 +21,28 @@ if __name__ == '__main__':
 
     # Now put the forward and left/right code into a for loop to repeat 4 times.
     # TEST    Did your turtle draw a square?
-
+    sally.color('yellow')
+    sally.pencolor('black')
+    sally.begin_fill()
+    for i in range(4):
+        sally.forward(50)
+        sally.right(90)
+    sally.end_fill()
     # Move your turtle to a new place on the screen using .goto(x, y)
     # x=0 and y=0 is the center of the screen
-
+    x = 0
+    y = 0
+    sally.goto(x, y)
     # Have your turtle draw a circle using .circle(radius, steps=30)
     # TEST    Did your turtle draw a circle?
-
     # Add color to your shape by adding .begin_fill() before drawing the circle
     # and .end_fill() below
-
+    sally.color('purple')
+    sally.begin_fill()
+    sally.circle(radius=150, steps=30)
+    sally.end_fill()
+    sally.color('green')
+    sally.pencolor('blue')
     # Draw 3 more shapes with different fill colors!
 
     # ===================== DO NOT EDIT THE CODE BELOW ============================
