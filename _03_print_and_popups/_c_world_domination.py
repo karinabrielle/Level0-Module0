85112d889559d7fbc57958e01012eaf99394a7ba
league <karinabriellen@gmail.com>
2023-10-08 13:00:28 -0700

    interesting game :0

--- _03_print_and_popups/_c_world_domination.py
+++ _03_print_and_popups/_c_world_domination.py
@@ -1,7 +1,8 @@
 from tkinter import messagebox, simpledialog, Tk
 
-# Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
+    # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
+if __name__ == '__main__':
+
     # Make a new window variable, window = Tk()
     window = Tk()
     # Hide the window using the window's .withdraw() method
@@ -10,10 +11,12 @@ from tkinter import messagebox, simpledialog, Tk
     code = simpledialog.askstring(title=" ", prompt="Do you know how to write code?" )
     # 2. If they say "yes", tell them they will rule the world in a message box pop-up.
     if code == "yes":
-        messagebox.showinfo(title=" ",message="You will rule the world!")
+        messagebox.showinfo(title=" ",message="YOU WILL RULE THE WORLD!")
     if code == "no":
-        messagebox.showerror(title=" ",message="Sign up for classes at The League!!!")
+        messagebox.showerror(title=" ",message="SIGN UP FOR CLASSES AT THE LEAGUE!!!")
         
     # 3. Otherwise, tell them to sign up for classes at The League in an error box pop-up.
-    else:
+    else: messagebox.showerror(title=" ", message="What did you say?")
+
     # Run the window's .mainloop() method
+    window.mainloop()
