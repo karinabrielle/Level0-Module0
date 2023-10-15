847e7278915952f71f3ce3b19f192e2fa90efc46
karinabrielle <143962082+karinabrielle@users.noreply.github.com>
2023-10-15 13:09:07 -0700

    Update _d_quiz_game.py

--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -11,24 +11,43 @@ if __name__ == '__main__':
     # 1. Create a variable to hold the user's score. Set it equal to zero. 
     score = 0
     # ASK A QUESTION AND CHECK THE ANSWER
-    question = simpledialog.askstring
-    correct = (title=" ", prompt= ' CORRECT!')
     #      // 2.  Ask the user a question 
     answer1 = simpledialog.askstring(title=" ", prompt="What is the name of an orange butterfly with black stripes on it's wings?")
     #      // 3.  Use an if statement to check if their answer is correct
     if answer1 == "monarch":
-        messagebox.showinfo(title=" ", prompt= ' CORRECT!')
+        messagebox.showinfo(title=" ", message= ' CORRECT!')
         score = score + 1
     #      // 4.  if the user's answer was correct, add one to their score 
-    else : score = score - 1
+    else :
+        score = score - 1
+        messagebox.showinfo(title=" ", message='SORRY, INCORRECT!')
     # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
     #      // Option: Subtract a point from their score for a wrong answer
-    answer2 = question(title= '', prompt= 'What is the first color of the rainbow?')
+    answer2 = simpledialog.askstring(title= '', prompt= 'What is the first color of the rainbow?')
     if answer2 == 'red' :
-        question(correct)
+        messagebox.showinfo(title=" ", message= ' CORRECT!')
         score = score + 1
-    else : score = score - 1
+    else :
+        score = score - 1
+        messagebox.showinfo(title=" ", message='SORRY, INCORRECT!')
+    answer3 = simpledialog.askstring(title = '', prompt= 'What is the most quickest and fastest land animal on earth?')
+    if answer3 == 'cheatah' :
+        messagebox.showinfo(title=" ", message= ' CORRECT!')
+        score = score + 1
+    else :
+        messagebox.showinfo(title=" ", message= 'SORRY, INCORRECT!')
+        score = score - 1
+    answer4 = simpledialog.askstring(title= '', prompt= 'Is chess a sport?')
+    if answer4 == 'yes':
+        messagebox.showinfo(title=" ", message= ' CORRECT!')
+        score = score + 1
+    else:
+        messagebox.showinfo(title=" ", message= 'SORRY, INCORRECT!')
+        score = score - 1
     # After all the questions have been asked, tell the user their final score
     # remember to convert your variable to a string using the str() function 
-    
+    messagebox.showinfo(title="", message="Here is your score :"
+    "(tap the ok button to see your score!)")
+    messagebox.showinfo(title="", message= score)
     # Run the window's .mainloop() method
+
