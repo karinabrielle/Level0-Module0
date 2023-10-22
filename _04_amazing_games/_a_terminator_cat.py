538d1e73bd0f47c32d633f1ca4c1740e54077a74
league <karinabriellen@gmail.com>
2023-10-22 12:58:10 -0700

    cat stuff

--- _04_amazing_games/_a_terminator_cat.py
+++ _04_amazing_games/_a_terminator_cat.py
@@ -45,10 +45,11 @@ def key_pressed():
 
     # 10. Increment the x and y variables of the 2 eye variables by 5:
     #     left_eye.x += 5
-    
+    left.x += 5
+    right.x += 5
     # 11. Call the .draw() method for both eye variables.
-
-
+    left.draw()
+    right.draw()
 if __name__ == '__main__':
     window = turtle.Screen()
     
@@ -61,16 +62,16 @@ if __name__ == '__main__':
     
     # 2. Call the set_background() function with your variable inside of the parenthesis
     #    for example, set_background(bg_image)
-
+    set_background('big_eyed_cat_gif_c.gif')
     # 3. Make a new turtle
-
+    butter=turtle.Turtle()
     # 4. Set the turtle color and pen color to red (or any color you want)
     #    using .color('red', 'red')
-
+    butter.color('red', 'red')
     # 5. Set the turtle width to 0 so no outlines are drawn
-
+    butter.width(0)
     # 6. Set the turtle speed to 0 (fastest)
-
+    butter.speed(0)
     # 7. Run the program and click on one of the cat's eyes. 
     #    The x,y position of the eye will be printed at the bottom of your
     #    processing window.
@@ -82,10 +83,11 @@ if __name__ == '__main__':
     #    and initialize them:
     #    left_eye  = Eye(eye=my_turtle, x=-34, y=11, radius=30)
     #    right_eye = Eye(eye=my_turtle, x=40, y=-5, radius=30)
-
+    left = Eye(eye=butter, x=30.0, y=-39.0, radius = 20)
+    right = Eye(eye=butter, x=-44.0, y=-5.0, radius=20)
     # 9. Call the .draw() method on BOTH eye variables
-
-
+    left.draw()
+    right.draw()
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
     window.onkeypress(key_pressed, 'space')
--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -7,19 +7,21 @@ if __name__ == '__main__':
     window.withdraw()
 
     # 1. Change this line to give you a random number between 1 - 100.
-    random_num = random.randint(1, 10)
+    random_num = random.randint(1, 100)
 
     # 2. Print out the random variable that you made in step #1
-
+    print(random_num)
     # 3. Code a for loop to run steps 4-10, 10 times
-
+    for i in range(10):
         # 4. Ask the user for a guess using a pop-up window, and save their response
-
+        answer = simpledialog.askinteger(title="", prompt= "I am thinking of a number that is from 1 to 100, what is my number?")
         # 5. If the guess is correct
             # 6. Win. Use 'sys.exit(0)' to end the program
-
+        sys.exit(0)
         # 7. if the guess is high
             # 8. Tell them it's too high
+            if
+                messagebox.showinfo(title= "", message= "your guess is too high!")
         # 9. Else if the guess is low
             # 10. Tell them it's too low
 
